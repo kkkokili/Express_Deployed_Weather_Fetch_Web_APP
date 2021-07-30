bfdcde722388bdd16c5fa9163e27b0d3064ac848
kokili <kokili.developer@outlook.com>
2021-07-30 16:36:32 +0800

    heroku

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ app.post('/', (req, res) => {
 });
 
 
-app.listen(process.env.Port|| 3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
   console.log('Port 3000 starts to listen!');
 });
